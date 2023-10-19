c3d095a338fbab9162916150f9dd339ddd470868
Diego <moronvillanuevadiego@outlook.com>
2023-10-19 02:36:03 -0500

    Fin sección 9

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,36 +1,55 @@
 const { response, request } = require('express')
+const bcryptjs = require('bcryptjs')
 
-const usuariosGet = (req = request, res = response) => {
-  const { q, nombre = 'No name', apikey } = req.query
+const Usuario = require('../models/usuario')
+
+const usuariosGet = async (req = request, res = response) => {
+  const { limite = 5, desde = 0 } = req.query
+  const query = { estado: true }
+
+  const [total, usuarios] = await Promise.all([
+    Usuario.countDocuments(query),
+    Usuario.find(query)
+        .skip(desde)
+        .limit(limite)
+  ])
   res.json({
-    msg: 'get API - Controlador',
-    q,
-    nombre,
-    apikey,
+    total,
+    usuarios,
   })
 }
 
-const usuariosPut = (req, res = response) => {
+const usuariosPut = async (req, res = response) => {
   const { id } = req.params
-  res.json({
-    msg: 'put API - Controlador',
-    id,
-  })
+  const { _id, password, google, correo, ...resto } = req.body
+  if(password) {
+    const salt = bcryptjs.genSaltSync()
+    resto.password = bcryptjs.hashSync(password, salt)
+  }
+  const usuario = await Usuario.findByIdAndUpdate( id, resto)
+  res.json(usuario)
 }
 
-const usuariosPost = (req, res = response) => {
-  const { nombre, edad } = req.body
+const usuariosPost = async (req, res = response) => {
+  const { nombre, correo, password, rol } = req.body
+  const usuario = new Usuario({nombre, correo, password, rol})
+  //Encriptar la contraseña
+  const salt = bcryptjs.genSaltSync()
+  usuario.password = bcryptjs.hashSync(password, salt)
+  //Guardar en base de datos
+  await usuario.save()
   res.json({
     msg: 'post API - Controlador',
-    nombre,
-    edad,
+    usuario
   })
 }
 
-const usuariosDelete = (req, res = response) => {
-  res.json({
-    msg: 'delete API - Controlador',
-  })
+const usuariosDelete = async (req, res = response) => {
+  const { id } = req.params
+  //Borramos fisicamente
+  // const usuario = await Usuario.findByIdAndDelete(id)
+  const usuario = await Usuario.findByIdAndUpdate(id, { estado: false })
+  res.json(usuario)
 }
 
 const usuariosPatch = (req, res = response) => {
---
+++ database/config.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_CNN)
+    console.log('Base de datos Online')
+  } catch (error) {
+    console.log(error)
+    throw new Error('Error en la conexión de la base de datos')
+  }
+}
+
+module.exports = {
+  dbConnection,
+}
